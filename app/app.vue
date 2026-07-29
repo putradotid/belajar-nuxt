48b5c793bc39df2a764f18e36314c4cb20aacc58
putradotid <aprildwi999@gmail.com>
2026-07-29 22:32:36 +0700

    setup tailwin

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h2 class="text-4xl font-black">Welcome to My Nuxt App</h2>
   </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
